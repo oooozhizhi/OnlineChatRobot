822340f4a0cdb0e0bd41f602936b22421467911a
igoingdown <fycjmingxing@126.com>
2017-06-23 00:46:01 +0800

    now the ajax test part can run smoothly. Now we can send info from front-end to back-end  and commit time: 2017-06-23 00:46:01.

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^blogApp/investigate', views.investigate, name='investigate'),
     url(r'^blogApp/train_ticket', views.stores, name='train'),
     url(r'^blogApp/flight', views.flight, name='flight'),
+    url(r'^comments_upload/$', views.comments_upload, name="comments_upload"),
+    url(r'^ajax_test/$', views.show_ajax_test_form, name="add"),
 ]
